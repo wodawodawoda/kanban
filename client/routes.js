1adf6343e11f67e322d0bd2efd9d9855cb5aca9e
Mateusz Wodnik <mateusz.wodnik@gmail.com>
2018-05-22 21:12:18 +0200

    Fix routes

--- client/routes.js
+++ client/routes.js
@@ -1,34 +1,53 @@
-/* eslint-disable global-require */
-import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-import App from './modules/App/App';
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeJsPlugin = require('optimize-js-plugin');
+const path = require('path');
 
-// require.ensure polyfill for node
-if (typeof require.ensure !== 'function') {
-  require.ensure = function requireModule(deps, callback) {
-    callback(require);
-  };
+const env = process.env.NODE_ENV || 'development';
+const plugins = [
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body',
+  }),
+  new ExtractTextPlugin('style.css'),
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+      sourceMap: false,
+    })
+  );
 }
 
-/* Workaround for async react routes to work with react-hot-reloader till
-  https://github.com/reactjs/react-router/issues/2182 and
-  https://github.com/gaearon/react-hot-loader/issues/288 is fixed.
- */
-if (process.env.NODE_ENV !== 'production') {
-  // Require async routes only in development for react-hot-reloader to work.
-  require('./modules/Kanban/Kanban');
-}
-
-// react-router setup with code-splitting
-// More info: http://blog.mxstbr.com/2016/01/react-apps-with-pages/
-export default (
-  <Route path="/" component={App}>
-    <IndexRoute
-      getComponent={(nextState, cb) => {
-        require.ensure([], require => {
-          cb(null, require('./modules/Kanban/Kanban').default);
-        });
-      }}
-    />
-  </Route>
-);
+module.exports = {
+  entry: [
+    'react-hot-loader/patch',
+    './src/index.js',
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.sass$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins,
+};
